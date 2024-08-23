1423c5a3d4a890acc247815f41c152662a85a327
nitin <nitin@gmail.com>
2024-08-23 22:47:38 +0530

    updated-day-5

--- day-5/main.tf
+++ day-5/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
     ami = "ami-02b49a24cfb95941c"
     instance_type = "t2.micro"
-    availability_zone = "ap-south-1b"
+    availability_zone = "ap-south-1a"
     key_name = "mumbai"
     
   tags = {
@@ -10,9 +10,9 @@ resource "aws_instance" "test" {
 
 #below examples are for lifecycle meta_arguments 
 
-  lifecycle {
-    create_before_destroy = true    #this attribute will create the new object first and then destroy the old one
-  }
+  # lifecycle {
+  #   create_before_destroy = true    #this attribute will create the new object first and then destroy the old one
+  # }
 
 # lifecycle {
 #   prevent_destroy = true   #Terraform will error when it attempts to destroy a resource when this is set to true:
